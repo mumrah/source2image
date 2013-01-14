7d1602546b3ea25961289ec7253fcae85b214e24
David Arthur <mumrah@gmail.com>
2013-01-14 23:28:00 +0000

    Adding standalone LaTeX module

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from cStringIO import StringIO
 import os
 import shlex
+import shutil
 import subprocess
 import tempfile
 
 from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -17,9 +17,14 @@ def render():
     lang = request.form['lang']
     source = request.form['source']
     wd = tempfile.mkdtemp()
+
     latexBody = render_template("source-listing.tex", lang=lang, source=source)
     inFile = StringIO(latexBody)
-    p1 = subprocess.Popen(["pdflatex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
+    standalone = os.path.join(os.getcwd(), "standalone")
+    env = os.environ
+    env["TEXINPUTS"] = ".:%s:" % standalone
+    p1 = subprocess.Popen(["pdflatex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+           cwd=wd, shell=True, env=env)
     (stdout, stderr) = p1.communicate(latexBody)
 
     p2args = shlex.split("convert -density 300 texput.pdf -quality 90 texput.png")
@@ -30,8 +35,10 @@ def render():
     resp = make_response(png)
     resp.headers["Content-Type"] = "image/png"
     #resp.headers["Content-Disposition"] = "attachment; filename=listing.png"
+
+    shutil.rmtree(wd)
     return resp
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
