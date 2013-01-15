f4f28cc2b7dc950180767868f8b40aa855a47cb1
David Arthur <mumrah@gmail.com>
2013-01-15 13:56:43 +0000

    Improve logging

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from cStringIO import StringIO
+import logging
+from logging import Formatter, DEBUG, INFO
+from logging.handlers import RotatingFileHandler
 import os
 import shlex
 import shutil
@@ -9,12 +12,24 @@ from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
 
 if not app.debug:
-    import logging
-    import logging.handlers
-    handler = logging.handlers.RotatingFileHandler("logs/app.log", maxBytes=1024*1024*32, backupCount=10)
-    handler.setLevel(logging.DEBUG)
+    # INFO logs
+    handler = RotatingFileHandler("logs/info.log", maxBytes=1024*1024*32, backupCount=10)
+    handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+    handler.setLevel(INFO)
     app.logger.addHandler(handler)
-    app.logger.info("OK")
+
+    # DEBUG logs
+    handler = RotatingFileHandler("logs/debug.log", maxBytes=1024*1024*32, backupCount=10)
+    handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+    handler.setLevel(DEBUG)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(DEBUG)
 
 languages = ["Python", "Java", "C"]
 
@@ -37,10 +52,14 @@ def render():
     p1 = subprocess.Popen(["pdflatex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
            cwd=wd, shell=True, env=env)
     (stdout, stderr) = p1.communicate(latexBody)
+    app.logger.debug("pdflatex stdout: %s" % stdout)
+    app.logger.debug("pdflatex stderr: %s" % stderr)
 
     p2args = shlex.split("convert -density 300 texput.pdf -quality 90 texput.png")
     p2 = subprocess.Popen(p2args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
     (stdout, stderr) = p2.communicate()
+    app.logger.debug("convert stdout: %s" % stdout)
+    app.logger.debug("convert stderr: %s" % stderr)
 
     png = open(os.path.join(wd, "texput.png"), "r").read()
     resp = make_response(png)
