635fa18c6436ca4764bb28f115396ff96c8f2df9
David Arthur <mumrah@gmail.com>
2013-01-15 14:58:24 +0000

    More logging improvements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cStringIO import StringIO
+from itertools import count
 import logging
 from logging import Formatter, DEBUG, INFO
 from logging.handlers import RotatingFileHandler
@@ -7,6 +8,7 @@ import shlex
 import shutil
 import subprocess
 import tempfile
+import time
 
 from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
@@ -15,7 +17,7 @@ if not app.debug:
     # INFO logs
     handler = RotatingFileHandler("logs/info.log", maxBytes=1024*1024*32, backupCount=10)
     handler.setFormatter(Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
+        '%(asctime)s %(process)d %(levelname)s: %(message)s '
         '[in %(pathname)s:%(lineno)d]'
     ))
     handler.setLevel(INFO)
@@ -24,7 +26,7 @@ if not app.debug:
     # DEBUG logs
     handler = RotatingFileHandler("logs/debug.log", maxBytes=1024*1024*32, backupCount=10)
     handler.setFormatter(Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
+        '%(asctime)s %(process)d %(levelname)s: %(message)s '
         '[in %(pathname)s:%(lineno)d]'
     ))
     handler.setLevel(DEBUG)
@@ -55,10 +57,14 @@ def render():
     lang = request.form['lang']
     source = request.form['source']
     mode = request.form['mode']
+    correlation_id = hash(frozenset([lang, source, mode, time.time()])) 
     assert lang in languages
 
+    app.logger.info("[%d] Begin handle of request" % correlation_id)
+    app.logger.debug("[%d] Handling request for lang: %s, source: %s" % (correlation_id, lang, source))
+
     wd = tempfile.mkdtemp()
-    app.logger.info("Creating temp directory %s" % wd)
+    app.logger.info("[%d] Creating temp directory %s" % (correlation_id, wd))
 
     latexBody = render_template("source-listing.tex", lang=lang, source=source)
     inFile = StringIO(latexBody)
@@ -68,14 +74,18 @@ def render():
     p1 = subprocess.Popen(["pdflatex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
            cwd=wd, shell=True, env=env)
     (stdout, stderr) = p1.communicate(latexBody)
-    app.logger.debug("pdflatex stdout: %s" % stdout)
-    app.logger.debug("pdflatex stderr: %s" % stderr)
+    if p1.returncode != 0:
+        app.logger.error("[%d] pdflatex had an error, check debug logs" % correlation_id)
+        app.logger.debug("[%d] pdflatex stdout: %s" % (correlation_id, stdout))
+        app.logger.debug("[%d] pdflatex stderr: %s" % (correlation_id, stderr))
 
     p2args = shlex.split("convert -density 300 texput.pdf -quality 90 texput.png")
     p2 = subprocess.Popen(p2args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
     (stdout, stderr) = p2.communicate()
-    app.logger.debug("convert stdout: %s" % stdout)
-    app.logger.debug("convert stderr: %s" % stderr)
+    if p2.returncode != 0:
+        app.logger.error("[%d] convert had an error, check debug logs" % correlation_id)
+        app.logger.debug("[%d] convert stdout: %s" % (correlation_id, stdout))
+        app.logger.debug("[%d] convert stderr: %s" % (correlation_id, stderr))
 
     png = open(os.path.join(wd, "texput.png"), "r").read()
     resp = make_response(png)
@@ -84,8 +94,9 @@ def render():
     if mode == "download":
         resp.headers["Content-Disposition"] = "attachment; filename=%s-listing.png" % lang.lower()
 
-    app.logger.info("Removing %s" % wd)
+    app.logger.info("[%d] Removing %s" % (correlation_id, wd))
     shutil.rmtree(wd)
+    app.logger.info("[%d] End handle of request" % correlation_id)
     return resp
 
 if __name__ == "__main__":
