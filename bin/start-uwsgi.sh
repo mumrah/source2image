8669e3a6c0b800589e8251cae6cfc8cc94fcfa83
David Arthur <mumrah@gmail.com>
2013-01-16 02:02:21 +0000

    Only one child process

--- bin/start-uwsgi.sh
+++ bin/start-uwsgi.sh
@@ -2,4 +2,4 @@
 APP_HOME=`dirname "$0"`/..
 cd $APP_HOME
 mkdir -p $APP_HOME/logs
-uwsgi_python --pidfile /tmp/uwsgi.pid -s /tmp/uwsgi.sock -w main:app -p 2 -M -d ./logs/uwsgi.log
+uwsgi_python --pidfile /tmp/uwsgi.pid -s /tmp/uwsgi.sock -w main:app -p 1 -M -d ./logs/uwsgi.log
