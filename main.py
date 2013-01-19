47b52a6bb2be7caf33c9706a11d18aee839b0275
David Arthur <mumrah@gmail.com>
2013-01-18 20:25:34 -0500

    Cleaning up code quite a bit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cStringIO import StringIO
 from itertools import count
 import logging
-from logging import Formatter, DEBUG, INFO
+from logging import Formatter, StreamHandler, DEBUG, INFO
 from logging.handlers import RotatingFileHandler
 import os
 import shlex
@@ -10,32 +10,33 @@ import subprocess
 import tempfile
 import time
 
-from webcolors import hex_to_rgb
 from flask import Flask, abort, request, render_template, make_response
 
 import extras
+from render import render_pdf, convert
 
 app = Flask(__name__)
 
+# Configure logging
 if not app.debug:
+    logger = logging.getLogger("source2Image")
+    logger.setLevel(DEBUG)
+    app.logger.setLevel(DEBUG)
+    formatter = Formatter('%(asctime)s %(process)d %(levelname)s: [%(correlation_id)s] '
+                          '%(message)s [in %(pathname)s:%(lineno)d]')
     # INFO logs
     handler = RotatingFileHandler("logs/info.log", maxBytes=1024*1024*32, backupCount=10)
-    handler.setFormatter(Formatter(
-        '%(asctime)s %(process)d %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-    ))
+    handler.setFormatter(formatter)
     handler.setLevel(INFO)
     app.logger.addHandler(handler)
+    logger.addHandler(handler)
 
     # DEBUG logs
     handler = RotatingFileHandler("logs/debug.log", maxBytes=1024*1024*32, backupCount=10)
-    handler.setFormatter(Formatter(
-        '%(asctime)s %(process)d %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-    ))
+    handler.setFormatter(formatter)
     handler.setLevel(DEBUG)
     app.logger.addHandler(handler)
-    app.logger.setLevel(DEBUG)
+    logger.addHandler(handler)
 
 # Need a special Jinja env for dealing with LaTeX
 texenv = app.create_jinja_environment()
@@ -59,41 +60,10 @@ languages = ('Python', 'Ruby', 'Java', 'C', 'C++', 'SQL', 'XML', 'HTML',
 
 formats = ("png", "pdf", "jpg")
 
-def convert_args(filename, alpha=False, background='#FFF'):
-    args = shlex.split("convert -density 200 texput.pdf")
-    if background:
-        args += shlex.split("-background '%s'" % background)
-    if not alpha:
-        args += shlex.split("-flatten")
-    args += shlex.split("-quality 90 %s" % filename)
-    return args
-
-class logger(object):
-    @classmethod
-    def debug(cls, msg, *args, **kwargs):
-        msg = "[%s] %s" % (app.extensions['correlation_id'], msg)
-        app.logger.debug(msg, *args, **kwargs)
-
-    @classmethod
-    def info(cls, msg, *args, **kwargs):
-        msg = "[%s] %s" % (app.extensions['correlation_id'], msg)
-        app.logger.info(msg, *args, **kwargs)
-
-    @classmethod
-    def warn(cls, msg, *args, **kwargs):
-        msg = "[%s] %s" % (app.extensions['correlation_id'], msg)
-        app.logger.warn(msg, *args, **kwargs)
-
-    @classmethod
-    def error(cls, msg, *args, **kwargs):
-        msg = "[%s] %s" % (app.extensions['correlation_id'], msg)
-        app.logger.error(msg, *args, **kwargs)
-        
-
 @app.route("/")
 def index():
     return render_template("index.html", 
-        source=open("main.py").read(),
+        source="print(\"Hello, World\")",
         languages=languages,
         formats=formats,
         extra_head=extras.head,
@@ -107,6 +77,7 @@ def robots():
 
 @app.route("/render", methods=["POST"])
 def render():
+    t1 = time.time()
     lang = request.form['lang']
     source = request.form['source']
     mode = request.form['mode']
@@ -119,10 +90,12 @@ def render():
     # Generate a correlation id for debugging
     app.extensions['correlation_id'] = hash(request_params)
 
+    # Wrap the logger with some request context
+    logger = logging.getLogger("source2Image")
+    logger = logging.LoggerAdapter(logger, app.extensions)
+
     assert fmt in formats
     assert lang in languages
-
-    background_rgb = hex_to_rgb(background)
 
     logger.info("Begin handle of request")
 
@@ -137,49 +110,33 @@ def render():
     latexBody = latexTmpl.render(lang=lang, source=source)
 
     # Generate PDF
-    standalone = os.path.join(os.getcwd(), "standalone")
-    env = os.environ
-    env["TEXINPUTS"] = ".:%s:" % standalone
-    p1 = subprocess.Popen(["pdflatex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-           cwd=wd, shell=True, env=env)
-    (stdout, stderr) = p1.communicate(latexBody.encode('latin-1'))
-    if p1.returncode != 0:
-        logger.error("pdflatex had an error, check debug logs")
-        logger.debug("pdflatex stdout: %s" % stdout)
-        logger.debug("pdflatex stderr: %s" % stderr)
+    pdfPath = render_pdf(latexBody, wd)
+    if not pdfPath:
+        abort(503)
 
+    # Determine the correct output, convert with ImageMagick if necessary
     if fmt == "pdf":
-        fileBytes = open(os.path.join(wd, "texput.pdf"), "r").read()
+        fileBytes = open(pdfPath, "r").read()
         contentType = "application/pdf"
         fileName = "%s-listing.pdf" % lang.lower()
-    # Convert with ImageMagick if request
     elif fmt == "png":
-        args = convert_args("texput.png", alpha=alpha, background=background)
-        p2 = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
-        (stdout, stderr) = p2.communicate()
-        if p2.returncode != 0:
-            logger.error("convert had an error, check debug logs")
-            logger.debug("convert stdout: %s" % stdout)
-            logger.debug("convert stderr: %s" % stderr)
+        outPath = convert(pdfPath, "texput.png", wd, alpha=alpha, background=background)
+        if not outPath:
             abort(503)
         else:
-            fileBytes = open(os.path.join(wd, "texput.png"), "r").read()
+            fileBytes = open(outPath, "r").read()
             contentType = "image/png"
             fileName = "%s-listing.png" % lang.lower()
     elif fmt == "jpg":
-        args = convert_args("texput.jpg", alpha=False, background=background)
-        p2 = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
-        (stdout, stderr) = p2.communicate()
-        if p2.returncode != 0:
-            logger.error("convert had an error, check debug logs")
-            logger.debug("convert stdout: %s" % stdout)
-            logger.debug("convert stderr: %s" % stderr)
+        outPath = convert(pdfPath, "texput.jpg", wd, alpha=False, background=background)
+        if not outPath:
             abort(503)
         else:
-            fileBytes = open(os.path.join(wd, "texput.jpg"), "r").read()
+            fileBytes = open(outPath, "r").read()
             contentType = "image/jpeg"
             fileName = "%s-listing.jpg" % lang.lower()
 
+    # Make the response
     resp = make_response(fileBytes)
     resp.headers["Content-Type"] = contentType
 
@@ -188,8 +145,10 @@ def render():
 
     logger.info("Removing %s" % wd)
     shutil.rmtree(wd)
-    logger.info("End handle of request")
+    t2 = time.time()
+    logger.info("End handle of request. Finished in %0.2fs" % (t2-t1))
     return resp
 
 if __name__ == "__main__":
+    logging.basicConfig(level=DEBUG)
     app.run(host="0.0.0.0", debug=True)
